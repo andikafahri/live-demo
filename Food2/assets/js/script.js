c95f3534cac727890f8ea14c3d877d2b346e83b1
Andika Fahri <andikafahri260@gmail.com>
2025-12-26 13:53:19 +0700

    Update food2

--- Food2/assets/js/script.js
+++ Food2/assets/js/script.js
@@ -1,4 +1,4 @@
-fetch('components/header.html').then(res => res.text()).then(html => {
+fetch('Food2/components/header.html').then(res => res.text()).then(html => {
 	document.getElementById('header').innerHTML = html
 
 	// Button Menu
@@ -33,6 +33,6 @@ fetch('components/header.html').then(res => res.text()).then(html => {
 	// })
 })
 
-fetch('components/footer.html').then(res => res.text()).then(html => {
+fetch('Food2/components/footer.html').then(res => res.text()).then(html => {
 	document.getElementById('footer').innerHTML = html
 })
